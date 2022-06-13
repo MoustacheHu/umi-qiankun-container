d386904587e5a7927d45a7db48621a5c4781da21
Hu <hsz@lxbznkj.com>
2022-06-13 14:23:54 +0800

    feat: init

---
+++ .umirc.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+      path: '/',
+      component: '@/pages/index'
+    },
+    {
+      path: '/app1',
+      microApp: 'app1'
+    },
+  ],
+  fastRefresh: {},
+  qiankun: {
+    master: {
+      // 注册子应用信息
+      apps: [
+        {
+          name: 'app1', // 唯一 id
+          entry: '//localhost:8000', // html entry
+        },
+      ],
+    },
+  },
+});
---
+++ src/pages/layout/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContainerLayout = () => {
+  return (
+    <div>
+      安安定定
+    </div>
+  );
+};
+
+export default ContainerLayout;
